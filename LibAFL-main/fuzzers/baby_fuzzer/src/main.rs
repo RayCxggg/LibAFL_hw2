dc1ab8cfda0fc7bcaa83dadb9a70e93c91a12254
RayCxggg <chexj18@163.com>
2022-11-30 03:09:41 +0000

    add monitor&Eventmanager&scheduler

--- LibAFL-main/fuzzers/baby_fuzzer/src/main.rs
+++ LibAFL-main/fuzzers/baby_fuzzer/src/main.rs
@@ -1,11 +1,11 @@
 extern crate libafl;
 use libafl::{
     bolts::AsSlice,
-    inputs::{BytesInput, HasTargetBytes},
     executors::ExitKind,
+    inputs::{BytesInput, HasTargetBytes},
 };
 
-fn main(){
+fn main() {
     let mut harness = |input: &BytesInput| {
         let target = input.target_bytes();
         let buf = target.as_slice();
@@ -22,4 +22,31 @@ fn main(){
     let input = BytesInput::new(Vec::from("abc"));
     #[cfg(feature = "panic")]
     harness(&input);
+
+    // create a State from scratch
+    let mut state = StdState::new(
+        // RNG
+        StdRand::with_seed(current_nanos()),
+        // Corpus that will be evolved, we keep it in memory for performance
+        InMemoryCorpus::new(),
+        // Corpus in which we store solutions (crashes in this example),
+        // on disk so the user can get them after stopping the fuzzer
+        OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
+        &mut (),
+        &mut (),
+    )
+    .unwrap();
+
+    // The Monitor trait defines how the fuzzer stats are displayed to the user
+    let mon = SimpleMonitor::new(|s| println!("{}", s));
+
+    // The event manager handle the various events generated during the fuzzing loop
+    // such as the notification of the addition of a new item to the corpus
+    let mut mgr = SimpleEventManager::new(mon);
+
+    // A queue policy to get testcasess from the corpus
+    let scheduler = QueueScheduler::new();
+
+    // A fuzzer with feedbacks and a corpus scheduler
+    let mut fuzzer = StdFuzzer::new(scheduler, (), ());
 }
