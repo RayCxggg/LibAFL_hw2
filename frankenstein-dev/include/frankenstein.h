1d295cab6857857a0d30d2fead3498d8805ea5c8
RayCxggg <chexj18@163.com>
2022-12-24 02:52:44 +0000

    init fran-dev

---
+++ frankenstein-dev/include/frankenstein.h
@@ -0,0 +1,15 @@
+#ifndef FRANKENSTEIN_H
+#define FRANKENSTEIN_H
+
+#include <frankenstein_config.h>
+
+#include <frankenstein/utils.h>
+#include <frankenstein/hook.h>
+#include <frankenstein/lgcc.h>
+
+#ifdef FRANKENSTEIN_EMULATION
+//#include <frankenstein/xmit_state_emu.h>
+#endif
+
+
+#endif
